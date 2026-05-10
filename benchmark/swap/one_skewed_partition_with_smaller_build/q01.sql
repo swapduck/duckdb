4bb18ddfc1520ceb199ff1042de3c971027e3e1a
SandaliLiyanage <pldsandalo@gmail.com>
2026-05-10 11:19:07 +0000

    bug fix

--- benchmark/swap/one_skewed_partition_with_smaller_build/q01.sql
+++ benchmark/swap/one_skewed_partition_with_smaller_build/q01.sql
@@ -1,9 +1,7 @@
-# 100MB Memory Limit, Single Threaded, Force Join Order, Enabled Adaptive Side Swapping
-
-SET disable_adaptive_side_swapping=true
+-- 100MB Memory Limit, Single Threaded, Force Join Order, Enabled Adaptive Side Swapping
 
 PRAGMA enable_profiling='json';
-PRAGMA profiling_output='benchmark/swap/one_skewed_partition_with_smaller_build/out/profile_q02.json';
+PRAGMA profiling_output='benchmark/swap/one_skewed_partition_with_smaller_build/out/profile_q01.json';
 
 PRAGMA custom_profiling_settings='{
     "SYSTEM_PEAK_BUFFER_MEMORY": "true",
